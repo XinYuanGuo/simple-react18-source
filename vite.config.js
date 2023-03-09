61e1eb594eb063e99fdb5a98a9e7414c5dd22c46
guoxinyuan <guoxy@flatincbr.com>
2023-03-09 10:47:23 +0800

    chore: 初始化

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import reactPlugin from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      react: path.posix.resolve("src/react"),
+      "react-dom": path.posix.resolve("src/react-dom"),
+      "react-reconciler": path.posix.resolve("src/react-reconciler"),
+      scheduler: path.posix.resolve("src/scheduler"),
+      shared: path.posix.resolve("src/shared"),
+    },
+  },
+  plugins: [reactPlugin()],
+});
